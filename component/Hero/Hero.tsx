0f56847167b3cddbd7686de72e6c202df373b37c
= <mdabutahersaikat@gmail.com>
2022-05-23 14:01:51 +0600

    work is going on

--- component/Hero/Hero.tsx
+++ component/Hero/Hero.tsx
@@ -1,26 +1,37 @@
 import Image from "next/image";
 import React from "react";
-import HeroImage from '../../public/img/hero.jpg';
+import { CgMail } from 'react-icons/cg';
+import styles from './hero.module.css';
 
 function Hero() {
   return (
     <div>
-      <div className="wrapper">
-        <div className="left-text-content">
-          <div className="style_block__TK6qm">
-            <h1>Freelancing made simple</h1>
-            <p>Indy helps you manage your freelance business. Send proposals, sign contracts, track billable hours, create invoices, and receive payments all in one place.
+      <div className={styles.wrapper}>
+
+        {/* left content section */}
+        <div className={styles.leftContentSection}>
+          <div className={styles.textSection}>
+            <h1 className={styles.topHeading}>Freelancing made simple</h1>
+            <p className={styles.topParagraph}>Indy helps you manage your freelance business. Send proposals, sign contracts, track billable hours, create invoices, and receive payments all in one place.
             </p>
-            <div className="style_withError__Xg9Gh">
-              <div className="style_container__0qlCb">
-                <div className="style_inputWrapper__7WKKs">
-                  {/* <image src="/icons/social/mail-outlined.svg" alt="Mail icon" /> */}
-                  <input type="email" className="style_input__5K4zr" placeholder="Enter your email..." value="" />
-                </div>
-                <button className="style_button__rc1MS style_button__JPLJp">Start your free trial</button></div></div></div>
+
+            <div className={styles.topInputWrapper}>
+              <div className={styles.innerInputWrapper}>
+                <CgMail className={styles.mailIcon} />
+                <input className={styles.topInput} type="email" placeholder="Enter your email.." />
+              </div>
+              <button className={styles.inputButton}>Start your free trail</button>
+            </div>
+
+          </div>
         </div>
+
+        {/* right image section */}
         <div className="right-image-content">
-          <Image src={HeroImage}></Image>
+          <Image
+            src="/img/hero-image.webp"
+            width={500}
+            height={500} />
         </div>
       </div>
     </div>
--- component/Trusted/Trusted.tsx
+++ component/Trusted/Trusted.tsx
@@ -1,7 +1,39 @@
+import Image from "next/image";
 import React from "react";
+import styles from './trusted.module.css';
 
 function Trusted() {
-  return <div>Trusted saikat</div>;
+  return <div>
+    <div className={styles.leftLogoSection}>
+      <h3 className={styles.trastedHeading}>Trusted by...</h3>
+      <div className={styles.row}>
+        <div className={styles.row1}>
+          <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+            <Image src='/img/trust-logo-1.webp' layout="fill" />
+          </div>
+          <div>
+            <Image src='/img/trust-logo-2.webp' width='100px' height='30px' />
+          </div>
+          <div>
+            <Image src='/img/trust-logo-3.webp' width='100px' height='30px' />
+          </div>
+        </div>
+
+        <div className={styles.row2}>
+          <div>
+            <Image src='/img/trust-logo-4.webp' width='100px' height='30px' />
+          </div>
+          <div>
+            <Image src='/img/trust-logo-5.webp' width='100px' height='30px' />
+          </div>
+          <div>
+            <Image src='/img/trust-logo-6.webp' width='100px' height='30px' />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="rightVideoSection"></div>
+  </div>;
 }
 
 export default Trusted;
